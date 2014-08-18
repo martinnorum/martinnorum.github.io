481b987a185e2303e355ee913aeeaf26433a39d7
Martin Engelsøy Norum <martin.norum@gmail.com>
2014-08-18 17:26:33 +0200

    Cleanup

--- Gruntfile.js
+++ Gruntfile.js
@@ -29,7 +29,7 @@ module.exports = function(grunt) {
     watch: {
 
       sass: {
-        files: ['sass/*.scss'],
+        files: ['sass/**/*.scss'],
         tasks: ['compass']
       },
       css: {

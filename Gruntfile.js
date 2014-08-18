751d298037bd9aa726441988a7c14118cbbacf40
Martin Engelsøy Norum <martin.norum@gmail.com>
2014-08-18 14:22:30 +0200

    Added content and got up and running with grunt and compass

---
+++ Gruntfile.js
@@ -0,0 +1,55 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    compass: {
+      dist: {
+        options: {
+          sassDir: 'css',
+          cssDir: 'css',
+          config: 'config.rb'
+        }
+      }
+    },
+
+    connect: {
+      all: {
+        port: 8000,
+        protocol: 'http',
+        hostname: 'localhost',
+        base: '.',
+        directory: null,
+        keepalive: false,
+        debug: false,
+        livereload: true,
+        open: true
+      }
+    },
+
+    watch: {
+
+      sass: {
+        files: ['css/*.scss'],
+        tasks: ['compass']
+      },
+      css: {
+        files: ['*.css'],
+        options: { livereload: true }
+      },
+      html: {
+        files: ['*.html'],
+        options: { livereload: true }
+      },
+      livereload: {
+        files: ['css/*.css'],
+        options: { livereload: true }
+      },
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-compass');
+  grunt.loadNpmTasks("grunt-contrib-watch");
+
+  grunt.registerTask("default", ['connect', "watch"]);
+};
